a6d2c96c5e2990fb14a910eca0ca532d3f6ad3df
Ly Thua An <andurex5555@gmail.com>
2026-02-01 07:34:26 +0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Thay 'ten-repo-cua-ban' bằng tên repository thực tế trên GitHub của bạn
+  base: 'MusicPro.com',
+  server: {
+    port: 3000
+  }
+});
