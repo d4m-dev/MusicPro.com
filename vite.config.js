992aa9c3728e7db3175f90eb1820f2cedda978b0
Ly Thua An <andurex5555@gmail.com>
2026-02-02 09:21:19 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // Thay 'ten-repo-cua-ban' bằng tên repository thực tế trên GitHub của bạn
-  base: 'MusicPro.com',
-  server: {
-    port: 3000
-  }
-});
