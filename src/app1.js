63b68513d0134e57514922664e57b294b3346e37
Ly Thua An <andurex5555@gmail.com>
2026-05-22 02:33:43 +0700

    Update and rename app.js to app1.js

--- src/app1.js
+++ src/app1.js
@@ -1,4 +1,4 @@
-const TRACKS_URL = 'src/tracks.js';
+const TRACKS_URL = 'https://raw.githubusercontent.com/d4m-dev/MusicPro.com/main/src/tracks.js';
 
 const normalizeTracks = (items = []) => items.map((item) => ({
     id: item.id,
